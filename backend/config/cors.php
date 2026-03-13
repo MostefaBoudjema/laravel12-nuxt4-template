e639d874dc97f5b7d7ae1d8464af12aa92f41402
mostfa <myreal17@gmail.com>
2026-03-13 03:30:58 +0100

    fix login

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,17 +1,26 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    //   'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -21,5 +30,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    // 'supports_credentials' => true,
+    'supports_credentials' => false, // Set to false when using '*' for allowed_origins
+
 ];
