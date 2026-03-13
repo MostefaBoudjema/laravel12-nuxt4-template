4b6efdab5b19cbcc0f64dcc6f8557915a1306562
mostfa <myreal17@gmail.com>
2026-03-13 13:48:09 +0100

    add versioning

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://laravel_saas.test/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://laravel_saas.test/api/v1'
     }
   },
 
